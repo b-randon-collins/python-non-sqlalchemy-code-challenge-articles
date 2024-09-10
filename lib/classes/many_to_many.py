f303bf5237931d214b00607238eff7f6a77f7eb3
Brandon Collins <brandonc503@yahoo.com>
2024-09-10 18:46:22 -0400

    magazine is of type Magazine and mutable

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,103 @@
 class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+    def __init__(self, author, magazine, title: str):
+        if not isinstance(author, Author) or not isinstance(magazine, Magazine):
+            raise ValueError("author must be an instance of Author and magazine must be an instance of Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters")
         
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise ValueError("new_author must be an instance of Author")
+        self._author = new_author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise ValueError("new_magazine must be an instance of Magazine")
+        self._magazine = new_magazine
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = name
+        self._articles = []
 
-    def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+    @property
+    def name(self):
+        return self._name
 
     def add_article(self, magazine, title):
-        pass
-
-    def topic_areas(self):
-        pass
-
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(magazine, Magazine):
+            raise ValueError("magazine must be an instance of Magazine")
+        
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def articles(self):
-        pass
+        return self._articles
 
-    def contributors(self):
-        pass
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+    def topic_areas(self):
+        return list(set(article.magazine.category for article in self._articles)) if self._articles else None
+
+
+class Magazine:
+    def __init__(self, name: str, category: str):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Category must be a non-empty string")
+        
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    def add_article(self, article):
+        if not isinstance(article, Article):
+            raise ValueError("article must be an instance of Article")
+        self._articles.append(article)
+
+    def articles(self):
+        return self._articles
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles] if self._articles else []
 
-    def contributing_authors(self):
-        pass
+    def contributors(self):
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author not in author_counts:
+                author_counts[author] = 0
+            author_counts[author] += 1
+
+        return [author for author, count in author_counts.items() if count > 2]
--- lib/testing/article_test.py
+++ lib/testing/article_test.py
@@ -47,8 +47,8 @@ class TestArticle:
         #     Article(author, magazine, "Test")
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
+        with pytest.raises(Exception):
+            Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
 
     def test_has_an_author(self):
         """article has an author"""
